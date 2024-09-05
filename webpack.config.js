c1f8cd09a5a77cf20b85660efabd7ea9c3621fe7
Emerson Kelly <123923271+Emerson-Kelly@users.noreply.github.com>
2024-09-04 20:02:33 -0400

    Update 'static' option in devServe

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   devtool: 'inline-source-map',
   devServer: {
     static: {
-      directory: path.join(__dirname, 'public'),
+      directory: path.resolve(__dirname, 'dist'),
     },
     compress: true,
     port: 9000,
